8d7da8d29fffae2d1b07d2e35d65bc9770d53974
tiagovinicius10 <tiagoviniciuspa@gmail.com>
2024-09-03 11:46:51 -0300

    Tiago

---
+++ questao04.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite um numero: '))#pedindo o primeiro numero
+n2 = int(input('digite outro numero: '))#pedindo o segundo numero
+
+if n1 > n2:
+    print("o numero 1 é maior que o 2")#verificando se o primeiro numero é maior que o segundo
+elif n1 < n2: 
+    print("o numero 1 é menor que o 2")#verificando se o primeiro numero é menor que o segundo
+else: 
+    print("os numeros sao iguais")#verificando se os numeros sao iguais
